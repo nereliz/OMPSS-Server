78cfc12bb5088dc01c50b1bc80d143172138061d
Nerijus Baraukas <nerijus@desktop.(none)>
2013-03-24 19:15:03 +0000

    [IM] type change

--- ompss/qtCompiler.sh
+++ ompss/qtCompiler.sh
@@ -1,1 +1,13 @@
-/usr/local/OMPSS.git/OMPSS/qtCompiler.sh
+#!/bin/bash
+
+args=("$@");
+
+cd ${args[0]}
+#echo `/usr/bin/sudo ${args[1]} -project`
+#echo `/usr/bin/sudo ${args[1]}`
+#echo `/usr/bin/sudo ${args[2]}`
+
+echo `${args[1]} -project`
+echo `${args[1]}`
+echo `${args[2]}`
+
