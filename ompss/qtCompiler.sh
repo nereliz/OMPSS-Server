ac37a7dedb9c7638d67b5b1d7237f93d2a035563
Nerijus Baraukas <nerijus@desktop.(none)>
2013-03-27 23:37:27 +0000

    [BF] Reverting back file typ

--- ompss/qtCompiler.sh
+++ ompss/qtCompiler.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-args=("$@");
-
-cd ${args[0]}
-#echo `/usr/bin/sudo ${args[1]} -project`
-#echo `/usr/bin/sudo ${args[1]}`
-#echo `/usr/bin/sudo ${args[2]}`
-
-echo `${args[1]} -project`
-echo `${args[1]}`
-echo `${args[2]}`
-
+/home/nerijus/workspace/OMPSS-Server.git/OMPSS/qtCompiler.sh
